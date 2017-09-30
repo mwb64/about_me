24a69dafedac34fd3f23af4d5a6e683353eaaf42
Michael Brown <mbrown488@gmail.com>
2017-09-30 14:32:34 -0700

    added 5th y/n question

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 var userScore = 0;
 
+//QUESTION 1 - USER'S Name
 var user = prompt('Hi stranger, I\'m Michael. Who might I have the honor of addressing?');
 alert('Welcome  ' + user + '! I assume your here because you want to get to know me! Soooo....lets make this introduction fun with a guessing game!!!');
 console.log('You\'ll be joined by ' + user + ' for this game.');
 
+//QUESTION 2 - yes/no: Is Michael's from Washington. Adjusts response to lower case.
 var whereFrom = prompt('Was Michael born in Washington? Y/N?').toLowerCase();
 console.log('Q2. Was Mike born in Washington? User says: ' + whereFrom + '.');
 
@@ -18,6 +20,7 @@ if (whereFrom === 'no' || whereFrom === 'n') {
 
 alert('You\'ve guessed correctly ' + userScore + ' time.');
 
+//QUESTION 3 - yes/no: Does Michael have any pets. Admjusts response to lower case.
 var petStatus = prompt('Does Mike have any pets? Y/N?').toLowerCase();
 console.log('Q3. Does Mike have any pets? User says: ' + petStatus + '.');
 
@@ -30,6 +33,7 @@ if (petStatus === 'no' || petStatus === 'n' ) {
 
 alert('You\'ve guessed correctly ' + userScore + ' times.');
 
+//QUESTION 4 - yes/no: Would Michael become a martian. Adjusts response to lower case.
 var martian = prompt('Would Mike volunteer to live on Mars? Y/N?').toLowerCase();
 console.log('Q4. Mike wants to be a Martian? User says: ' + martian + '.');
 
@@ -41,6 +45,7 @@ if (martian === 'yes' || martian === 'y' ) {
 }
 alert('You\'ve guessed correctly ' + userScore + ' times.');
 
+//QUESTION 5 - yes/no: Does michael enjoy coding. Adjusts response to lower case.
 var codeLove = prompt('Does Mike enjoy coding? Y/N?').toLowerCase();
 console.log('Q4. Mike enjoys coding at this point? User says: ' + codeLove + '.');
 
@@ -50,4 +55,18 @@ if (codeLove === 'yes' || codeLove === 'y' ) {
   alert('So far he does.....so far...ask me again later.');
   userScore = 0;
 }
-alert('You\'ve guessed correctly ' + userScore + ' times!');
+
+alert('You\'ve guessed correctly ' + userScore + ' times.');
+
+//QUESTION 6 - yes/no: Does michael enjoy coding. Adjusts response to lower case.
+var pieLove = prompt('Does Mike love pie more than cake? Y/N?').toLowerCase();
+console.log('Q4. Mike enjoys pie? User says: ' + pieLove + '.');
+
+if (pieLove === 'yes' || pieLove === 'y' ) {
+  userScore++;
+} else {
+  alert('Of course he does! Pie is obvously better than cake.');
+  userScore = 0;
+};
+
+alert('Thanks for playing ' + user + 'you scored ' + userScore + '.');
